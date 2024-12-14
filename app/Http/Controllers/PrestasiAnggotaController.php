583e3cf754d5062d055cc81268f004ee31e38fda
devanify <yanevan03@gmail.com>
2024-12-14 13:07:32 +0800

    feat: create prestasi anggota

--- app/Http/Controllers/PrestasiAnggotaController.php
+++ app/Http/Controllers/PrestasiAnggotaController.php
@@ -4,21 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Anggota;
 use App\Models\Periode;
-use App\Models\PrestasiAnggota;
 use Illuminate\Http\Request;
+use App\Models\PrestasiAnggota;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class PrestasiAnggotaController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $maxdata = 10;
         $periode = Periode::all();
         $prestasi = PrestasiAnggota::paginate(10);
         $anggota = Anggota::all();
-        
+
         return view('prestasi_anggota.index', compact('periode', 'prestasi', 'anggota'));
     }
 
-    public function create(Request $request){
+    public function create(Request $request)
+    {
         return view('prestasi_anggota.create');
     }
 
@@ -26,17 +30,63 @@ class PrestasiAnggotaController extends Controller
     {
         if ($request->ajax()) {
             $search = $request->get('search');
-            $anggota = Anggota::where('nama', 'like', "%{$search}%")
-                            ->orWhere('nim', 'like', "%{$search}%")
-                            ->get();
+            $anggota = Anggota::with('periode')
+                ->where('nama', 'like', "%{$search}%")
+                ->orWhere('nim', 'like', "%{$search}%")
+                ->get();
 
             return response()->json($anggota);
         }
     }
 
 
-    public function store(Request $request){
-        
-    }
+    public function store(Request $request)
+    {
+       $request->validate([
+            'id_anggota' => 'required|exists:anggota,id',
+            'nama_prestasi' => 'required|string|max:255',
+            'tingkat' => 'required|in:lokal,nasional,internasional',
+            'tahun_prestasi' => 'required|digits:4|integer|before_or_equal:' . date('Y'),
+            'keterangan' => 'nullable|string|max:1000',
+            'file' => 'required|file|mimes:jpeg,png,jpg|max:2048',
+        ], [
+            'id_anggota.required' => 'Anggota harus dipilih.',
+            'id_anggota.exists' => 'Anggota yang dipilih tidak ditemukan di database.',
+            'nama_prestasi.required' => 'Nama prestasi harus diisi.',
+            'nama_prestasi.string' => 'Nama prestasi harus berupa teks.',
+            'nama_prestasi.max' => 'Nama prestasi maksimal 255 karakter.',
+            'tingkat.required' => 'Tingkat prestasi harus dipilih.',
+            'tingkat.in' => 'Tingkat prestasi harus salah satu dari: lokal, nasional, atau internasional.',
+            'tahun_prestasi.required' => 'Tahun prestasi harus diisi.',
+            'tahun_prestasi.digits' => 'Tahun prestasi harus terdiri dari 4 digit.',
+            'tahun_prestasi.integer' => 'Tahun prestasi harus berupa angka.',
+            'tahun_prestasi.before_or_equal' => 'Tahun prestasi tidak boleh lebih dari tahun ini.',
+            'keterangan.string' => 'Keterangan harus berupa teks.',
+            'keterangan.max' => 'Keterangan maksimal 1000 karakter.',
+            'file.required' => 'Gambar harus diisi.',
+            'file.file' => 'File harus berupa file.',
+            'file.mimes' => 'File harus bertipe jpeg, png, jpg, atau pdf.',
+            'file.max' => 'Ukuran file tidak boleh lebih dari 2MB.',
+        ]);
 
+        if ($request->hasFile('file')) {
+            $originalFileName = $request->file('file')->getClientOriginalName();
+            $fileNameWithoutExtension = pathinfo($originalFileName, PATHINFO_FILENAME);
+            $formattedFileName = str_replace(' ', '_', $fileNameWithoutExtension);
+            $fileFinalName = 'prestasi-' . $formattedFileName . '-' . uniqid() . '.' . $request->file('file')->getClientOriginalExtension();
+            $request->file('file')->move(public_path('dokumen/prestasi_anggota/'), $fileFinalName);
+        }
+        $data = [
+            'id_anggota' => $request->id_anggota,
+            'nama_prestasi' => $request->nama_prestasi,
+            'tingkat' => $request->tingkat,
+            'tahun_prestasi' => $request->tahun_prestasi,
+            'keterangan' => $request->keterangan,
+            'file' => $fileFinalName,
+        ];
+        
+        PrestasiAnggota::create($data);
+        
+        return redirect()->route('prestasi_anggota.index')->with('success', 'Data berhasil ditambahkan.');
+    }
 }
---
+++ resources/views/prestasi_anggota/create.blade.php
@@ -0,0 +1,190 @@
+@extends('layout.layout')
+@section('page-title', 'Tambah Prestasi UKM')
+
+@section('content')
+    <div class="row mb-4">
+        <div class="mb-lg-0 mb-4">
+            <div class="col-lg-12">
+                <div class="card p-3">
+                    <div class="container-fluid px-0">
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col-lg-9">
+                                    <h6>Tambah Prestasi Anggota</h6>
+                                </div>
+                                <div class="col-lg-3">
+                                    <form id="searchForm" class="d-flex">
+                                        <input type="text" id="searchInput" class="form-control" name="search"
+                                            placeholder="Cari Nama atau NIM..." aria-label="Kata kunci ...">
+                                        <button class="btn shadow-none mb-0 btn-outline-dark mx-1" type="button"
+                                            id="searchButton">
+                                            <i class="fas fa-search"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <!-- Tabel Hasil Pencarian -->
+                        <div id="anggotaTable" class="table-responsive">
+                            <!-- Hasil pencarian anggota akan muncul di sini -->
+                        </div>
+
+                        <!-- Form untuk menambahkan prestasi -->
+                        <form action="{{ route('prestasi_anggota.store') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group">
+                                <input type="text" name="id_anggota" id="id_anggota" class="form-control" value="{{ old('id_anggota') }}" hidden>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="nama_anggota">Nama Anggota</label>
+                                <input type="text" id="nama_anggota_disabled" class="form-control" 
+                                    value="{{ old('nama_anggota', $anggota->nama ?? '') }}" readonly disabled>
+                                <input type="hidden" name="nama_anggota" id="nama_anggota" 
+                                    value="{{ old('nama_anggota', $anggota->nama ?? '') }}">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="nim_anggota">NIM Anggota</label>
+                                <input type="text" id="nim_anggota_disabled" class="form-control" 
+                                    value="{{ old('nim_anggota', $anggota->nim ?? '') }}" readonly disabled>
+                                <input type="hidden" name="nim_anggota" id="nim_anggota" 
+                                    value="{{ old('nim_anggota', $anggota->nim ?? '') }}">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="prestasi">Nama Prestasi</label>
+                                <input type="text" name="nama_prestasi" id="prestasi" class="form-control" value="{{ old('nama_prestasi') }}" required>
+                                @error('nama_prestasi')
+                                        <div class="text-danger font-weight-bold text-xs mt-2">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        
+                            <div class="form-group">
+                                <label for="tahun_prestasi">Tahun Prestasi</label>
+                                <input type="number" name="tahun_prestasi" id="tahun_prestasi" class="form-control" value="{{ old('tahun_prestasi') }}" required>
+                                @error('tahun_prestasi')
+                                        <div class="text-danger font-weight-bold text-xs mt-2">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        
+                            <div class="form-group">
+                                <label for="tingkat">Tingkat Prestasi</label>
+                                <select name="tingkat" id="tingkat" class="form-control" required>
+                                    <option value="" disabled {{ old('tingkat') == null ? 'selected' : '' }}>Pilih Prestasi</option>
+                                    <option value="lokal" {{ old('tingkat') == 'lokal' ? 'selected' : '' }}>Lokal</option>
+                                    <option value="nasional" {{ old('tingkat') == 'nasional' ? 'selected' : '' }}>Nasional</option>
+                                    <option value="internasional" {{ old('tingkat') == 'internasional' ? 'selected' : '' }}>Internasional</option>
+                                </select>
+                                @error('tingkat')
+                                        <div class="text-danger font-weight-bold text-xs mt-2">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        
+                            <div class="form-group">
+                                <label for="keterangan">Keterangan</label>
+                                <textarea name="keterangan" id="keterangan" class="form-control" rows="3" placeholder="Masukkan keterangan (jika ada)" >{{ old('keterangan') }}</textarea>
+                                @error('keterangan')
+                                        <div class="text-danger font-weight-bold text-xs mt-2">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        
+                            <div class="form-group">
+                                <label for="file">Gambar</label>
+                                <input type="file" name="file" id="file" class="form-control" accept="image/*">
+                                <p class="text-secondary font-weight-bold text-xs mt-2"> Note : Maksimal ukuran file 2MB dengan format .jpg, .png atau .jpeg</p>
+                                @error('file')
+                                        <div class="text-danger font-weight-bold text-xs mt-2">{{ $message }}</div>
+                                @enderror
+                            </div>
+                                                    
+                            <button type="submit" class="btn btn-success">Simpan Prestasi</button>
+                        </form>
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Script untuk menangani AJAX Pencarian dan Pilih Anggota -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#searchButton').on('click', function() {
+                var search = $('#searchInput').val();
+
+                $.ajax({
+                    url: "{{ route('prestasi_anggota.search') }}",
+                    method: "GET",
+                    data: {
+                        search: search
+                    },
+                    success: function(data) {
+                        var html = `
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">NIM</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Periode</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Kelas</th>
+                                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>`;
+                        if (data.length > 0) {
+                            // Looping untuk setiap anggota
+                            data.forEach(function(anggota, index) {
+                                html += `
+                            <tr>
+                                <td class="text-xs align-middle text-center">${index + 1}</td>
+                                <td class="text-xs align-middle text-center">${anggota.nama}</td>
+                                <td class="text-xs align-middle text-center">${anggota.nim}</td>
+                                <td class="text-xs align-middle text-center">${anggota.periode ? anggota.periode.tahun : 'Tidak Ada'}</td>
+                                <td class="text-xs align-middle text-center">${anggota.kelas}</td>
+                                <td class="text-xs align-middle text-center">
+                                    <button class="btn btn-success btn-sm" 
+                                            onclick="selectAnggota(${anggota.id}, '${anggota.nama}', '${anggota.nim}')">
+                                        Pilih
+                                    </button>
+                                </td>
+                            </tr>
+                        `;
+                            });
+                        } else {
+                            html += `
+                        <tr>
+                            <td colspan="4" class="text-center">Anggota tidak ditemukan.</td>
+                        </tr>`;
+                        }
+
+                        html += `
+                        </tbody>
+                    </table>`;
+                        $('#anggotaTable').html(html);
+                    }
+                });
+            });
+        });
+
+
+        // Fungsi untuk memilih anggota dan mengisi form
+        function selectAnggota(id, nama, nim) {
+            // console.log('ID:', id);  
+            // console.log('Nama:', nama);  
+            // console.log('NIM:', nim); 
+            $('#id_anggota').val(id);
+            $('#nama_anggota').val(nama);
+            $('#nim_anggota').val(nim);
+            // Menampilkan Nama dan NIM di input yang readonly
+            $('#nama_anggota_disabled').val(nama);
+            $('#nim_anggota_disabled').val(nim);
+        }
+    </script>
+@endsection
